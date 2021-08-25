b8b9a3c7d36257bafead22a92df2b7b24129e74c
hajime nakamura <h.nakamura0903@gmail.com>
2021-08-26 00:00:34 +0900

    練習問題9.6.3

---
+++ ex9-3.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use v5.10;
+use strict;
+use warnings;
+use autodie;
+
+my $src = $ARGV[0];
+if (! defined $src) {
+  die "Usage: $0 filename";
+}
+
+my $dst = $src . '.out';
+open my $in, '<', $src;
+open my $out, '>', $dst;
+
+while (<$in>) {
+  s/fred/\0/gi;
+  s/wilma/Fred/gi;
+  s/\0/Wilma/gi;
+  print $out $_;
+}
+
+close $in;
+close $out;
