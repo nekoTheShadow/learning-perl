35c526fa7f1a5361bca5b5c76f82d2e0b1ba30ad
hajime nakamura <h.nakamura0903@gmail.com>
2021-08-25 23:27:49 +0900

    練習問題8.7.1

---
+++ ex8-1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use v5.10;
+use strict;
+use warnings;
+
+while (<>) {
+  chomp;
+  if (/match/) {
+    print "Matched: |$`<$&>$'|\n";
+  } else {
+    print "No Match: |$_|\n";
+  }
+}
